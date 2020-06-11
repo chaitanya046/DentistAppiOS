8f1bb08ece6a6dc2e88fee63cbadf924f05fe136
Chaitanya Uttarwar <chaitanyauttarwar046@gmail.com>
2020-06-11 00:33:10 -0400

    Initial Commit

---
+++ DentistApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DentistApp
+//
+//  Created by user174125 on 6/11/20.
+//  Copyright © 2020 Chaitanya Uttarwar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
