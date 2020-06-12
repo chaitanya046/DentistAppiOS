084550ea544d9f989dfa8d8a2fcc3274fd1ff334
Chaitanya Uttarwar <chaitanyauttarwar046@gmail.com>
2020-06-12 00:36:08 -0400

    added button connection

--- DentistApp/ViewController.swift
+++ DentistApp/ViewController.swift
@@ -10,13 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var btnClick: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        mj;
+        //let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        //let firstView = UIView(frame: firstFrame)
+        //firstView.backgroundColor = UIColor.blue
+        //view.addSubview(firstView)
+              btnClick.addTarget(self,action: #selector(tapOnButton), for: .touchUpInside)    }
+
+    @objc func tapOnButton(){
+        let story = UIStoryboard(name: "Main", bundle: nil)
+        let controller = story.instantiateViewController(identifier: "ViewController") as! ViewController
+        self.present(controller,animated: true,completion: nil )
     }
-
-
+    
+    @objc func tapOnButtonForNavigation(){
+        let story = UIStoryboard(name: "Main", bundle: nil)
+        let controller = story.instantiateViewController(identifier: "ViewController") as! ViewController
+        let navigation = UINavigationController(rootViewController: controller)
+        self.view.addSubview(navigation.view)
+        self.addChild(navigation)
+        navigation.didMove(toParent: self)
+    }
 }
 
 		
