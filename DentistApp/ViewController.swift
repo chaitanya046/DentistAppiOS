c78a00a5a18a1bed6691ea15bc081309106b679d
Melih Karakaya <mgkarakaya@gmail.com>
2020-06-13 02:55:19 -0400

    Alert edited, Datepicker works successfully

--- DentistApp/ViewController.swift
+++ DentistApp/ViewController.swift
@@ -31,30 +31,32 @@ class ViewController: UIViewController {
         //checks if all the inputs are filled or not
         if(name.isEmpty || age.isEmpty || phone.isEmpty){
             let alert = UIAlertController(title: "Information Not saved ", message: "Please fill all the input fields", preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
         else{
             //informaion is valid and filled correctly
              let alert = UIAlertController(title: "Information Saved", message: "Dear, "+name+" Aged: "+age+" you will be soon sent details on: "+phone, preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
-
-       
-
-        
     }
-    
+    var myDateString = "Date"
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        //getting date from the datepicker and assigning it to the variable which we will use in the alert dialog box
+          let dateFormatter = DateFormatter()
+
+    dateFormatter.dateStyle = DateFormatter.Style.short
+    dateFormatter.timeStyle = DateFormatter.Style.short
+
+     myDateString = dateFormatter.string(from: datepicker.date)
+       
+    }
     //executes when confirm button is pressed
     @IBAction func btnConfirm(_ sender: Any) {
-		//getting date from the datepicker and assigning it to the variable which we will use in the alert dialog box
-        let inputDate =  datepicker.date
-        let formatter = DateFormatter()
-        let myString = formatter.string(from: inputDate)
         //creates and displays the alert
-        let alert = UIAlertController(title: "Apointment Confirmed", message: "Your appointment is confirmed "+myString, preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+        let alert = UIAlertController(title: "Apointment Confirmed", message: "Your appointment is confirmed "+myDateString, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
                self.present(alert, animated: true, completion: nil)
         
         
