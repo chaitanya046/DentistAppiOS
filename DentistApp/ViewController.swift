37e709fffc5eaf104db85294c94907989d2e59fe
Chaitanya Uttarwar <chaitanyauttarwar046@gmail.com>
2020-06-13 02:27:51 -0400

    final commit for assignment 1

--- DentistApp/ViewController.swift
+++ DentistApp/ViewController.swift
@@ -21,27 +21,39 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var datepicker: UIDatePicker!
     
+    //executes when save button is pressed
     @IBAction func btnSave(_ sender: UIButton)
     {
 		//getting text values from the textfield and assigning them to the variables which we are using in the alert dialog box
         let name: String = nameEntered.text!
         let age: String = ageEntered.text!
         let phone: String = phoneEntered.text!
-        let alert = UIAlertController(title: "Information Saved", message: "Dear, "+name+" Aged: "+age+" you will be soon sent details on: "+phone, preferredStyle: UIAlertController.Style.alert)
+        //checks if all the inputs are filled or not
+        if(name.isEmpty || age.isEmpty || phone.isEmpty){
+            let alert = UIAlertController(title: "Information Not saved ", message: "Please fill all the input fields", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else{
+            //informaion is valid and filled correctly
+             let alert = UIAlertController(title: "Information Saved", message: "Dear, "+name+" Aged: "+age+" you will be soon sent details on: "+phone, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
 
-        alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+       
+
+        
     }
     
-    
+    //executes when confirm button is pressed
     @IBAction func btnConfirm(_ sender: Any) {
 		//getting date from the datepicker and assigning it to the variable which we will use in the alert dialog box
         let inputDate =  datepicker.date
         let formatter = DateFormatter()
         let myString = formatter.string(from: inputDate)
-        
-        
-        let alert = UIAlertController(title: "Apointment Confirmed", message: "Your appointment is confirmed for: "+myString, preferredStyle: UIAlertController.Style.alert)
+        //creates and displays the alert
+        let alert = UIAlertController(title: "Apointment Confirmed", message: "Your appointment is confirmed "+myString, preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
                self.present(alert, animated: true, completion: nil)
         
