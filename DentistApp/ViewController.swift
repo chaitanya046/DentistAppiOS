0c133a202f56331e265de9844cfa819534f84aa2
Melih Karakaya <mgkarakaya@gmail.com>
2020-06-12 21:45:57 -0400

    patient information, Map, confirmation screens are added into TabBar Layout

--- DentistApp/ViewController.swift
+++ DentistApp/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+   
+  
+
     @IBOutlet weak var btnClick: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
